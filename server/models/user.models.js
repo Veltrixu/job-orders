57cb6d90b7e4c766171b7a975f66f09bce2a5f4e
Jeff Ivan Mayor <jeffivanbiosanomayor@gmail.com>
2025-05-27 16:32:35 +0800

    Update user.models.js

--- server/models/user.models.js
+++ server/models/user.models.js
@@ -9,12 +9,6 @@ const UserSchema = new mongoose.Schema(
     lastname: {
       type: String,
       required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
     username: {
       type: String,
       required: true,
